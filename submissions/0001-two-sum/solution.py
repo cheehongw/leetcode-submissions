f5b822d9c8c64efd4a133047441cbe96f0caed18
Chee Hong <c.h.wong2606@gmail.com>
2024-07-05 06:05:37 +0000

    [LeetCode Sync] Runtime - 51 ms (92.39%), Memory - 18 MB (8.78%)

--- submissions/0001-two-sum/solution.py
+++ submissions/0001-two-sum/solution.py
@@ -1,16 +1,19 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        inv_map = {}
-        for idx, i in enumerate(nums):
-            inv = target - i
+        # approach:
+        # for each number x in the list, compute the other number y 
+        # that will sum up to the target
+        # store y in a hashmap so that we can find the idx of x
+        # as we iterate through the list, we will eventually encounter the second x
+        # that we have hashed into the map as a key
+        prevMap = {} 
 
+        for i, n in enumerate(nums):
+            diff = target - n
+            if n in prevMap:
+                return [i, prevMap[n]]
+            prevMap[diff] = i
 
-            if inv in inv_map:
-                return [inv_map[inv], idx]
-            else:
-                inv_map[i] = idx
-            
-            
-            
-            
         
+            
+
