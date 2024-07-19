24e5ea0e374ca41b85019d25d0f15b7d0014475f
Chee Hong <c.h.wong2606@gmail.com>
2024-07-19 12:13:16 +0000

    [LeetCode Sync] Runtime - 44 ms (17.44%), Memory - 16.6 MB (34.82%)

--- submissions/0021-merge-two-sorted-lists/solution.py
+++ submissions/0021-merge-two-sorted-lists/solution.py
@@ -3,17 +3,48 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        if (list1 and list2):
-            if (list1.val < list2.val):
-                list1.next = self.mergeTwoLists(list1.next, list2)
-                return list1
-            else:
-                list2.next = self.mergeTwoLists(list1, list2.next)
-                return list2
+        
+        newListHead = None
+        newListTail = None
 
-        elif (list1):
+        if (list1 is None and list2 is None):
+            return None
+        elif (list1 is None):
+            return list2
+        elif (list2 is None):
             return list1
         else:
-            return list2
+            if list1.val <= list2.val:
+                newListHead = list1 
+                newListTail = list1
+                list1 = list1.next
+            else:
+                newListHead = list2
+                newListTail = list2
+                list2 = list2.next
+
+        while (True):
+            if (list1 is None and list2 is None):
+                break
+
+            if (list1 is None):
+                newListTail.next = list2
+                break
+            
+            if (list2 is None):
+                newListTail.next = list1
+                break
+
+            if (list1.val <= list2.val):
+                newListTail.next = list1
+                newListTail = list1
+                list1 = list1.next
+            else:
+                newListTail.next = list2
+                newListTail = list2
+                list2 = list2.next                
+
+        return newListHead
