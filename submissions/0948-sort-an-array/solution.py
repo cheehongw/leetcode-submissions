f25816bd51bac2671576381c4dc0edd2cf29d26b
Chee Hong <c.h.wong2606@gmail.com>
2024-07-25 14:15:06 +0000

    [LeetCode Sync] Runtime - 584 ms (75.47%), Memory - 24 MB (70.99%)

---
+++ submissions/0948-sort-an-array/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        heapq.heapify(nums)
+
+        result = []
+
+        while(nums):
+            result.append(heapq.heappop(nums))
+
+        return result
