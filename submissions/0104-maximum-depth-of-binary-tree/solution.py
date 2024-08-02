13f934eed97d9fd39c68db9d76729ded1a6f1ed1
Chee Hong <c.h.wong2606@gmail.com>
2024-08-02 19:17:43 +0000

    [LeetCode Sync] Runtime - 30 ms (97.43%), Memory - 17.4 MB (94.84%)

--- submissions/0104-maximum-depth-of-binary-tree/solution.py
+++ submissions/0104-maximum-depth-of-binary-tree/solution.py
@@ -9,8 +9,52 @@
 # O(n) - time complexity
 # O(n) - space complexity
 class Solution:
-    def maxDepth(self, root: Optional[TreeNode]) -> int:
-        if (root is None):
-            return 0
+    # def maxDepth(self, root: Optional[TreeNode]) -> int:
+    #     if (root is None):
+    #         return 0
 
-        return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+    #     return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+
+
+# provide bfs soln
+
+    # def maxDepth(self, root: Optional[TreeNode]) -> int:
+    #     q = deque([root, None])
+    #     if root is None:
+    #         return 0
+
+    #     depth = 0
+    #     while q:
+    #         node = q.popleft()
+            
+    #         if (node is None):
+    #             depth += 1
+    #             if q:
+    #                 q.append(None)
+    #             continue
+
+    #         if (node.left):
+    #             q.append(node.left)
+            
+    #         if (node.right):
+    #             q.append(node.right)
+        
+    #     return depth
+
+# provide iterative dfs solution
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        stack = [(root, 1)]
+        maxD = 0
+
+        while (stack):
+            node, depth = stack.pop()
+            
+            if node:
+                stack.append((node.left, depth + 1))
+                stack.append((node.right, depth + 1))
+                maxD = max(maxD, depth)
+
+        return maxD
+
+            
+
