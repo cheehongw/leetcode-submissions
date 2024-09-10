bcf74aadf93e08c54ee5ab2e484da7305ab1e731
Chee Hong <c.h.wong2606@gmail.com>
2024-09-10 17:41:01 +0000

    [LeetCode Sync] Runtime - 82 ms (23.00%), Memory - 19.5 MB (80.13%)

--- submissions/2903-insert-greatest-common-divisors-in-linked-list/solution.py
+++ submissions/2903-insert-greatest-common-divisors-in-linked-list/solution.py
@@ -5,19 +5,32 @@
 #         self.next = next
 class Solution:
     def insertGreatestCommonDivisors(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if head is None:
-            return head
+        ptr = head
 
-        if head.next is None:
-            return head
-
-        GCD = gcd(head.val, head.next.val)
-
-        next_node = head.next
-        new_node = ListNode(GCD, next_node)
-        head.next = new_node
-
-        self.insertGreatestCommonDivisors(next_node)
-
+        while (ptr and ptr.next):
+            GCD = gcd(ptr.val, ptr.next.val)
+            new_node = ListNode(GCD, ptr.next)
+            curr_node = ptr
+            ptr = ptr.next
+            curr_node.next = new_node
+            
         return head
+            
+            
+
+        # if head is None:
+        #     return head
+
+        # if head.next is None:
+        #     return head
+
+        # GCD = gcd(head.val, head.next.val)
+
+        # next_node = head.next
+        # new_node = ListNode(GCD, next_node)
+        # head.next = new_node
+
+        # self.insertGreatestCommonDivisors(next_node)
+
+        # return head
         
