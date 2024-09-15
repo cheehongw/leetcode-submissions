1494e7128ea89785277780aea447e3cefe9c5608
Chee Hong <c.h.wong2606@gmail.com>
2024-09-15 02:31:50 +0000

    [LeetCode Sync] Runtime - 60 ms (100.00%), Memory - 16.5 MB (100.00%)

---
+++ submissions/3581-the-two-sneaky-numbers-of-digitville/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def getSneakyNumbers(self, nums: List[int]) -> List[int]:
+        seen = set()
+        sneaky = []
+
+        for num in nums:
+            if num in seen:
+                sneaky.append(num)
+
+            seen.add(num)
+
+        return sneaky
